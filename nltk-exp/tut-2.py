de5c48337a57fab8e5f284d56e6898142d249c97
ovshake <abhishek16005@iiitd.ac.in>
2017-06-24 17:49:42 +0530

    tuned stock price modelling for icicibank for this month

--- nltk-exp/tut-2.py
+++ nltk-exp/tut-2.py
@@ -1,7 +1,6 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk
-nltk.download()
 example_sentence = "This example shows off the stop words"
 stop_words = set(stopwords.words("english"))
 words = word_tokenize(example_sentence)
--- predective-stock-model/model.py
+++ predective-stock-model/model.py
@@ -8,10 +8,9 @@ def get_data(filename):
 	with open(filename,'r') as csvFile:
 		csvFileReader = csv.reader(csvFile)
 		next(csvFileReader)
-		print(cs)
 		for row in csvFileReader:
-			dates.append(int(row[0].split("-")[0]))
-			prices.append(float(row[1]))
+			dates.append(int(row[2].split("-")[0]))
+			prices.append(float(row[4]))
 	return
 def predict_prices(dates, prices, x):
 	dates = np.reshape(dates,(len(dates),1))
@@ -35,6 +34,7 @@ def predict_prices(dates, prices, x):
 
 	return svr_poly.predict(x)[0], svr_rbf.predict(x)[0], svr_lin.predict(x)[0] 
 
-get_data('aapl.csv')
-predicted_prices = predict_prices(dates, prices,29)
+get_data('icici2.csv')
+predicted_prices = predict_prices(dates, prices,21)
+print((predicted_prices[1] + predicted_prices[2])/2)
 print(predicted_prices)
